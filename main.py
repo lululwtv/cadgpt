f1da6d11a5411e9583dd361c107ae01368bc1dff
lululwtv <edrickhiewlol@gmail.com>
2025-01-14 00:51:35 -0800

    testing w cadquery documentation now

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from langchain_core.vectorstores import InMemoryVectorStore
 import bs4
 import os
 from langchain import hub
-from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.document_loaders import WebBaseLoader, PyPDFLoader
 from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langgraph.graph import START, StateGraph
@@ -29,50 +29,14 @@ embeddings = VertexAIEmbeddings(model="text-embedding-004")
 vector_store = InMemoryVectorStore(embeddings)
 
 # Load and chunk contents of the blog
-loader = WebBaseLoader(
-    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
-    bs_kwargs=dict(
-        parse_only=bs4.SoupStrainer(
-            class_=("post-content", "post-title", "post-header")
-        )
-    ),
-)
-docs = loader.load()
+file_path = "./documents/CadQuery Cheatsheet.pdf"
+loader = PyPDFLoader("./documents/CadQuery Cheatsheet.pdf")
+pages = []
+for page in loader.load():
+    pages.append(page)
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-all_splits = text_splitter.split_documents(docs)
+vector_store = InMemoryVectorStore.from_documents(pages, embeddings)
+docs = vector_store.similarity_search("what is this about")
 
-# Index chunks
-_ = vector_store.add_documents(documents=all_splits)
-
-# Define prompt for question-answering
-prompt = hub.pull("rlm/rag-prompt")
-
-
-# Define state for application
-class State(TypedDict):
-    question: str
-    context: List[Document]
-    answer: str
-
-
-# Define application steps
-def retrieve(state: State):
-    retrieved_docs = vector_store.similarity_search(state["question"])
-    return {"context": retrieved_docs}
-
-
-def generate(state: State):
-    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
-    messages = prompt.invoke({"question": state["question"], "context": docs_content})
-    response = llm.invoke(messages)
-    return {"answer": response.content}
-
-
-# Compile application and test
-graph_builder = StateGraph(State).add_sequence([retrieve, generate])
-graph_builder.add_edge(START, "retrieve")
-graph = graph_builder.compile()
-
-response = graph.invoke({"question": "What is Task Decomposition?"})
-print(response["answer"])
+for doc in docs:
+    print(f'Page {doc.metadata["page"]}: {doc.page_content[:300]}\n')
